7309e561cbbbb12f1b0a9a54d431c2c31515b16e
annoyc <yc2hangycz@gmail.com>
2026-06-04 11:01:37 +0800

    chore: adapt vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 import path from 'node:path'
 
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
   },
   plugins: [
     tanstackStart(),
+    nitro(),
     react(),
     tailwindcss(),
   ],
